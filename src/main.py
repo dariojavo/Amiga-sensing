8dd9fcff78de47ed252201f516d37087110aca23
Dario Guevara <dariojavo91@gmail.com>
2023-07-30 22:52:51 -0700

    add basic new interface functionality

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import asyncio
 import os
 from typing import List
 
-from amiga_package import ops
+from amiga_sensing import ops
 
 # import internal libs
 
@@ -24,6 +24,12 @@ Config.set("kivy", "keyboard_mode", "systemanddock")
 # kivy imports
 from kivy.app import App  # noqa: E402
 from kivy.lang.builder import Builder  # noqa: E402
+import numpy as np
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy_garden.mapview import MapView, MapMarker
 
 
 class TemplateApp(App):
@@ -33,11 +39,24 @@ class TemplateApp(App):
         super().__init__()
 
         self.counter: int = 0
-
+        self.marker = None  # Initialize the marker attribute
         self.async_tasks: List[asyncio.Task] = []
+        self.longitude = -122.4194
+        self.latitude = 37.7749 
 
     def build(self):
-        return Builder.load_file("res/main.kv")
+        root =  Builder.load_file("res/main.kv")
+        # Right half with a map view
+        self.mapview = root.ids.map_view
+
+        self.image = root.ids.image
+
+        # self.image.size_hint = (0.5, 1)
+        self.image.allow_stretch = True
+        self.image.keep_ratio = False
+
+        return root
+
 
     def on_exit_btn(self) -> None:
         """Kills the running kivy application."""
@@ -69,6 +88,49 @@ class TemplateApp(App):
             self.root.ids.counter_label.text = (
                 f"{'Tic' if self.counter % 2 == 0 else 'Tac'}: {self.counter}"
             )
+
+            # Update the noisy image and map marker
+            self.update_noisy_image()
+            self.update_gps_position()
+            
+
+    def update_noisy_image(self):
+        # Generate a random noisy image (300x300 with random values between 0 and 255)
+        noise_img = np.random.randint(0, 256, (1920, 1080, 3), dtype=np.uint8)
+
+        # Display the noisy image in the left half
+        self.display_image(noise_img)
+
+    def display_image(self, img_array):
+        # Convert NumPy array to Kivy Texture
+        h, w, _ = img_array.shape
+        texture = Texture.create(size=(w, h), colorfmt='rgb')
+
+        # Flip the image vertically (Kivy's image origin is bottom-left)
+        img_array = np.flipud(img_array)
+
+        # Copy the image data to the texture
+        texture.blit_buffer(img_array.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
+
+        # Set the texture for the Image widget
+        self.image.texture = texture
+
+    def update_gps_position(self):
+        # In this example, we'll use dummy GPS coordinates.
+        # You should replace these with real GPS coordinates if available.
+        self.latitude = self.latitude + 0.001  # San Francisco, CA, USA
+        self.longitude = self.longitude + 0.001
+        latitude = self.latitude
+        longitude = self.longitude
+        # Update the marker position on the map
+        if self.marker is not None:
+            self.mapview.remove_marker(self.marker)  # Remove previous marker
+        self.marker = MapMarker(lat=latitude, lon=longitude)
+        self.mapview.add_marker(self.marker)
+
+        # Center the map view on the current GPS position
+        self.mapview.center_on(latitude, longitude)
+        self.mapview.zoom = 15
 
 
 if __name__ == "__main__":
